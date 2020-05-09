b8d229fe08c27684ca73a2a404853cf67f53cbea
rajeevnaikte <rajeevnaikte@gmail.com>
2020-05-08 23:22:55 -0400

    add documentation

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -1,3 +1,8 @@
+/**
+ * Takes given configs JSON data and finds if there are any key value has been defined in
+ * env. If so will override those JSON key-value with the env value.
+ * @param configs
+ */
 export const loadConfigs = <T>(configs: T): T => {
   const envValues = Object.keys(configs).filter(key => process.env[key])
     .reduce((map: any, key) => {
