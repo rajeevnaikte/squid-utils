95d8fced25f60b26af87f88fef582dd7d8eab12c
rajeevnaikte <rajeevnaikte@gmail.com>
2022-01-17 15:38:19 -0500

    re-export type-fest

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './utils/utils';
 export * from './errors';
+export * from 'type-fest';
 export * from './types';
 export * from './utils/textParsers';
 export * from './utils/TextsBetween';
